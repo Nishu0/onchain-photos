a225dbdaa733b106b2689c9a80c6dc0d985c2ab6
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2025-06-15 15:29:40 +0530

    initial commit from @neynar/create-farcaster-mini-app

---
+++ src/app/providers.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import type { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { MiniAppProvider } from "@neynar/react";
+
+const WagmiProvider = dynamic(
+  () => import("~/components/providers/WagmiProvider"),
+  {
+    ssr: false,
+  }
+);
+
+export function Providers({ session, children }: { session: Session | null, children: React.ReactNode }) {
+  return (
+    <SessionProvider session={session}>
+      <WagmiProvider>
+        <MiniAppProvider analyticsEnabled={true}>
+            {children}
+        </MiniAppProvider>
+      </WagmiProvider>
+    </SessionProvider>
+  );
+}
